7d445e791b54704bd462a4569b68105b0d1f9077
Anisha_Rani <anisha.rani@dell.com>
2026-06-22 22:53:31 +0530

    [ER-29]:Flyweight design pattern

--- src/flyweightDP/solution/DocumentCharacter.java
+++ src/flyweightDP/solution/DocumentCharacter.java
@@ -3,6 +3,9 @@ package flyweightDP.solution;
 public class DocumentCharacter implements  Letter{
      //class will be immutable
     //keep all intrinsic data that can be shared across objects private and only add getters for them;
+    //can't make it static because static will overwrite the static values as one single memory location will be shared.
+    //incase of flyweight we are just avoiding creating obj itself if we already have in cache with same char and make class such that
+    //intrinsic data is not mutable(private and final only set once via constructor and no setter) and extrinsic data is mutable exposed via methods.
     private final char character;//intrinsic
     private final String fontType;//intrinsic
     private final int size;//intrinsic
